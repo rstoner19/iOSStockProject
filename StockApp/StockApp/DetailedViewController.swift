c92731493039cb46a5a496cd3415542bc6b4f904
Rick Stoner <stonerrick19@gmail.com>
2016-07-06 14:21:05 -0700

    updating detailed view

--- StockApp/StockApp/DetailedViewController.swift
+++ StockApp/StockApp/DetailedViewController.swift
@@ -11,6 +11,22 @@ import UIKit
 class DetailedViewController: UIViewController, Setup {
     
     @IBOutlet weak var symbolLabel: UILabel!
+    @IBOutlet weak var priceChangeLabel: UILabel!
+    @IBOutlet weak var currentPriceLabel: UILabel!
+    
+    //Detailed Price Compenonents
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var priceCurrentLabel: UILabel!
+    @IBOutlet weak var priceTimeLabel: UILabel!
+    @IBOutlet weak var priceBidAskLabel: UILabel!
+    @IBOutlet weak var priceDayHighLabel: UILabel!
+    @IBOutlet weak var priceDayLowLabel: UILabel!
+    @IBOutlet weak var priceDayRangeLabel: UILabel!
+    @IBOutlet weak var priceYearHighLabel: UILabel!
+    @IBOutlet weak var priceYearLowLabel: UILabel!
+    @IBOutlet weak var priceYearRangeLabel: UILabel!
+    
+    
     
     var quote: Quote?
 
@@ -26,11 +42,25 @@ class DetailedViewController: UIViewController, Setup {
         // Dispose of any resources that can be recreated.
     }
     
-    
     func setup() {
+        self.priceLabel.text = "Price"
         if let quote = self.quote {
+            // Main Section
             self.title = quote.company
             self.symbolLabel.text = quote.symbol
+            self.currentPriceLabel.text = "$\(quote.lastPrice!)"
+            
+            if quote.dollarChange > 0 {
+                self.priceChangeLabel.textColor = UIColor(colorLiteralRed: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
+                // Image
+            } else if quote.dollarChange < 0 {
+                self.priceChangeLabel.textColor = UIColor(colorLiteralRed: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+            }
+            self.priceChangeLabel.text = "$\(quote.dollarChange!)"
+            
+            //Price Section
+            self.priceLabel.text = "Cur. Price: \(quote.lastPrice)"
+            
         }
         
     }
